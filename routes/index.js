9687d31f41cb010d978782338bd32b81be2afae2
mwqdev <quarles.michael@gmail.com>
2017-08-09 18:13:30 -0400

    cleaner

--- routes/index.js
+++ routes/index.js
@@ -2,24 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    // if (!req.session || !req.session.authenticated) {
-    //     res.render('index', {title: 'Welcome to Gabble!', user: null, msg: 'Please log in to write gabs'});
-    // } else {
-    //     res.render('index', {title: 'Gabble', user: req.session.displayName});
-    // }
     res.render('portfolio');
 });
-
-// router.post('/gab', (req, res) => {
-//     let gab = models.Gab.build({
-//         userId: req.session.userId,
-//         title: req.body.gabTitle,
-//         body: req.body.gabBody
-//     });
-//
-//     gab.save().then((gab) => {
-//         res.redirect('/');
-//     });
-// });
 
 module.exports = router;
