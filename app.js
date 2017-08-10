2b618dc38d3124c971e4bb4d0e31500637c54a1f
mwqdev <quarles.michael@gmail.com>
2017-08-09 23:09:16 -0400

    satisfied

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const port = parseInt(process.env.PORT) || '3000';
 
@@ -9,6 +10,9 @@ const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,88 @@
 const express = require('express');
 const router = express.Router();
 
+const mongo = require('mongodb');
+const assert = require('assert');
+
+const url = 'mongodb://localhost:27017/userDatabase';
+
 router.get('/', (req, res) => {
-    res.render('portfolio');
+
+    mongo.connect(url, (err, db) => {
+
+        assert.equal(null, err);
+        let collection = db.collection('robots');
+
+        collection.find({}).toArray((err, result) => {
+            assert.equal(null, err);
+            let response = result[0].users;
+            res.render('index', {result: response});
+        });
+        db.close();
+    });
+});
+
+router.get('/single/:userId', (req, res) => {
+
+    let id = req.params.userId;
+
+    let findUser = function (user) {
+        return user.id == id;
+    };
+
+    mongo.connect(url, async (err, db) => {
+        assert.equal(null, err);
+        let collection = await db.collection('robots');
+
+        collection.find({}).toArray((err, result) => {
+            assert.equal(null, err);
+            let users = result[0].users;
+            let user = users.find(findUser);
+            res.render('single', user);
+        });
+        db.close();
+    });
+});
+
+router.get('/lfw', (req, res) => {
+
+    mongo.connect(url, (err, db) => {
+
+        assert.equal(null, err);
+        let collection = db.collection('robots');
+
+        collection.find({}).toArray((err, result) => {
+            assert.equal(null, err);
+            let jobless = [];
+            let response = result[0].users;
+            for (let i in response) {
+                if (!response[i].job)
+                    jobless.push(response[i]);
+            }
+            res.render('lfw', {users: jobless});
+        });
+        db.close();
+    });
+});
+
+router.get('/employed', (req, res) => {
+
+    mongo.connect(url, (err, db) => {
+        assert.equal(null, err);
+        let collection = db.collection('robots');
+
+        collection.find({}).toArray((err, result) => {
+            assert.equal(null, err);
+            let employed = [];
+            let response = result[0].users;
+            for (let i in response) {
+                if (response[i].job)
+                    employed.push(response[i]);
+            }
+            res.render('employed', {users: employed});
+        });
+        db.close();
+    });
 });
 
 module.exports = router;
